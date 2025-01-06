025ace6620f22740c54305eb20b4f8a97fc14271
Roland Steinegger <roland@steinegger.one>
2025-01-06 07:07:50 +0100

    Adds daisy ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/css/tailwind.css'],
